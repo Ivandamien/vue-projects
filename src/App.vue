b300a5e3288b23ae785e627b836bd933b9fd9f65
Ivandamien <ivandamieno99@gmail.com>
2025-01-15 15:24:51 +0300

    Working with composition api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
-<script>
-export default {
-  name: "App",
-  data() {
-    return {
-      name: "Ivan",
-      status: "active",
-      tasks: ["Task One", "Task Two", "Task Three"],
-      link: "https://www.google.com",
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === "active") {
-        this.status = "pending";
-      } else if (this.status === "pending") {
-        this.status = "inactive";
-      } else {
-        this.status = "active";
-      }
-    },
-  },
+<script setup>
+import { ref, onMounted } from "vue";
+
+const name = "Ivan";
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const link = "https://www.google.com";
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
 };
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
 <template>
@@ -28,9 +44,18 @@ export default {
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is Pending</p>
   <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" name="newTask" id="newTask" v-model="newTask" />
+    <button type="submit">Add Task</button>
+  </form>
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
   </ul>
   <!-- <a v-bind:href="link">Click for Google</a> -->
   <a :href="link">Click for Google</a>
