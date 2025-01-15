1ef586acd645982196e368bd5b6504eafe8aa32a
Ivandamien <ivandamieno99@gmail.com>
2025-01-15 14:12:00 +0300

    optional api

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,20 @@ export default {
     return {
       name: "Ivan",
       status: "active",
+      tasks: ["Task One", "Task Two", "Task Three"],
+      link: "https://www.google.com",
     };
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+      } else {
+        this.status = "active";
+      }
+    },
   },
 };
 </script>
@@ -15,4 +28,12 @@ export default {
   <p v-if="status === 'active'">User is Active</p>
   <p v-else-if="status === 'pending'">User is Pending</p>
   <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <a :href="link">Click for Google</a>
+  <br />
+  <button @click="toggleStatus">Change Status</button>
 </template>
