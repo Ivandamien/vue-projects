462d6c5f970202aa4f23833f71eea0f4527fbdac
Ivandamien <ivandamieno99@gmail.com>
2025-01-17 21:15:15 +0300

    Finishing up homepage

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -4,7 +4,14 @@ import jobData from "@/jobs.json";
 import { ref } from "vue";
 
 const jobs = ref(jobData.jobs);
-console.log(jobs.value);
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
 <template>
@@ -14,8 +21,19 @@ console.log(jobs.value);
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+        <JobListing
+          v-for="job in jobs.slice(0, limit || jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
       </div>
     </div>
+  </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
   </section>
 </template>
